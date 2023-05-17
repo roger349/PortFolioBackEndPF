0a1ab3a4d39a43bcdc2352fcecb928ee65dbac71
Roger Roldan <roger349@gmail.com>
2023-05-17 00:30:13 -0300

    commit 13 portfolio backend Ap

--- PortFolioBackEnd/src/main/java/com/rer/backend/PortFolioBackEndApplication.java
+++ PortFolioBackEnd/src/main/java/com/rer/backend/PortFolioBackEndApplication.java
@@ -2,6 +2,10 @@ package com.rer.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortFolioBackEndApplication {
@@ -9,5 +13,15 @@ public class PortFolioBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortFolioBackEndApplication.class, args);
 	}
-
+        @EnableWebMvc
+        @Configuration
+        public class WebConfiguracion implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+               registry.addMapping("/**").allowedOrigins("/**")
+                       .allowedMethods("*").allowedHeaders("*");
+        }
+}        
 }
+        
+
